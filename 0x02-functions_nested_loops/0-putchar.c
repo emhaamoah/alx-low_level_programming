626b741a6155118d5d472aa06dfe8857ef7a46fe
Emmanuel Amoah <emhaamoah@gmail.com>
2022-09-13 18:01:14 -0500

    removing while loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,22 @@
+
 #include "main.h"
 /**
- * main - Prints Holberton
+ * main - Prints _putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char *sh = "_putchar";
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
