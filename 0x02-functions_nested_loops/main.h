9a59e8329fc80276c565627a80a71d3d12f4c425
Emmanuel Amoah <111864073+emhaamoah@users.noreply.github.com>
2022-09-13 20:14:43 +0000

    updating main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -15,4 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif /* _HOLBERTON_H_ */
+#endif /* _MAIN_ */
